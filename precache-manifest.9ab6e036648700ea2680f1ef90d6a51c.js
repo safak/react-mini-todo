405cae161441a8f7415fe8a672c16b5a51e5ebd4
safak <safakkocaoglu@hotmail.com>
2020-06-06 00:11:47 +0200

    Updates

---
+++ precache-manifest.9ab6e036648700ea2680f1ef90d6a51c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092e35ecd988de657530ffce09859035",
+    "url": "/react-mini-todo/index.html"
+  },
+  {
+    "revision": "9a1ab4bbb6a96aa0cb51",
+    "url": "/react-mini-todo/static/css/main.8b65ef72.chunk.css"
+  },
+  {
+    "revision": "4cae1de56eb4510ac1b0",
+    "url": "/react-mini-todo/static/js/2.b2604ae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mini-todo/static/js/2.b2604ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1ab4bbb6a96aa0cb51",
+    "url": "/react-mini-todo/static/js/main.76a48fa4.chunk.js"
+  },
+  {
+    "revision": "e24931b806f01661a5f8",
+    "url": "/react-mini-todo/static/js/runtime-main.51df8c18.js"
+  }
+]);
